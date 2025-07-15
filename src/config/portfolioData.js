2652a41f9f8d0acc2a706dc4dd532daee7443c5e
melloj-dot <joseignaciocaramello@gmail.com>
2025-07-15 19:47:08 -0300

    fixed img path

--- src/config/portfolioData.js
+++ src/config/portfolioData.js
@@ -1,34 +1,34 @@
 export const projects = [
   {
     title: 'Michis',
-    image: '/img/portfolio/fullsize/1.png',
+    image: './img/portfolio/fullsize/1.png',
     description: 'Used technologies: ReactJS+Vite, Bootstrap, Bootswatch, Public API',
     link: 'https://github.com/caramelloj/utntpn2',
   },
   {
     title: 'Estimations & export .pdf',
-    image: '/img/portfolio/fullsize/2.png',
+    image: './img/portfolio/fullsize/2.png',
     description: 'Used technologies: ReactJS+Vite, Bootstrap,',
     link: 'https://github.com/usuario/proyecto1',
     isPrivate: true
   },
   {
     title: 'CRM for Lubricentro',
-    image: '/img/portfolio/fullsize/3.png',
+    image: './img/portfolio/fullsize/3.png',
     description: 'Used technologies: PHP, Laravel, HTML, CSS, Blade, AdminLTE v3, Bootstrap, MySql, GIT',
     link: 'https://github.com/usuario/proyecto1',
     isPrivate: true
   },
   {
     title: 'Food Reserves System',
-    image: '/img/portfolio/fullsize/4.png',
+    image: './img/portfolio/fullsize/4.png',
     description: 'Used technologies: PHP, Laravel, HTML, CSS, JS, Blade, AdminLTE v3, Bootstrap, MySql, GIT',
     link: 'https://github.com/usuario/proyecto1',
     isPrivate: true
   },
   {
     title: 'CRUD for car repairs',
-    image: '/img/portfolio/fullsize/5.png',
+    image: './img/portfolio/fullsize/5.png',
     description: 'Used technologies: PHP, Laravel, HTML, CSS, Blade, Bootstrap, MySql, GIT',
     link: 'https://github.com/caramelloj/tallerelectricidad',
   }

1f38bd9007e90bbef31175043d6b041badd9f33b
melloj-dot <joseignaciocaramello@gmail.com>
2025-07-19 21:26:42 -0300

    fix navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,29 +27,21 @@ export default function Navbar() {
   }, []);
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
-      <div className="container px-4 px-lg-5">
-        <a className="navbar-brand" href="/">Jose Caramello</a>
-        <button
-          className="navbar-toggler navbar-toggler-right"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarResponsive"
-          aria-controls="navbarResponsive"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarResponsive">
-          <ul className="navbar-nav ms-auto my-2 my-lg-0">
-            <li className="nav-item"><a className="nav-link" href="#about">{t('about')}</a></li>
-            <li className="nav-item"><a className="nav-link" href="#services">{t('services')}</a></li>
-            <li className="nav-item"><a className="nav-link" href="#portfolio">{t('portfolio')}</a></li>
-            <li className="nav-item"><a className="nav-link" href="#contact">{t('contact')}</a></li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+        <header className="masthead">
+          <div className="container px-4 px-lg-5 h-100">
+            <div className="row gx-4 gx-lg-5 h-100 align-items-center justify-content-center text-center">
+              <div className="col-lg-8 align-self-end">
+                <h1 className="text-white font-weight-bold">Jose Caramello</h1>
+                <hr className="divider" />
+              </div>
+              <div className="col-lg-8 align-self-baseline">
+                <p className="text-white-75 mb-5" id="animated-heading">
+                  <RotatingText prefix={isEnglish ? t('prefix') : ''} />
+                </p>
+                <a className="btn btn-primary btn-xl" href="#about">{t('button')}</a>
+              </div>
+            </div>
+          </div>
+        </header>
   );
 }
